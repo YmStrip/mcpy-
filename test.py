dd28dca19b169609c29bcbdbcdb4cdd19af287df
PomeloRs <418685307@qq.com>
2021-03-29 23:23:29 +0800

    上传文件

---
+++ test.py
@@ -0,0 +1,8 @@
+﻿from ym import ui
+
+def create ( self ):
+	jq = ui .load ( ui .get ( self , '/data' ) )
+	jq .show ( )
+	jq .find ( '#main!.button,.text+:/main:[:1]' ) .settext ( 'main' ) .setcolor ( '#ffffff' )
+	jq .find ( 'say[first]' ) .onclick ( '''click func''' ) .settext ( 'hello' )
+ui .create ( create )
---
+++ ym.py
@@ -0,0 +1,8 @@
+#Author: YmStrip
+#Hint: Unauthorized use is prohibited 
+#Github: github.com/YamStrip/mcpy/ym.py
+#Website: www.pomelors.com
+
+#Simple package 
+
+from gui import ui
